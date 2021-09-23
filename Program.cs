eb325a7c8dc88bdd52b079f611c3b627c819d1af
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-09-23 13:24:08 -0700

    trying path info

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
 
 namespace files_module
 {
@@ -6,7 +9,37 @@ namespace files_module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var salesFiles = FindFiles("stores");
+
+            foreach (var file in salesFiles)
+            {
+                Console.WriteLine(file);
+            }
+
+            string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+            FileInfo info = new FileInfo(fileName);
+
+            Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}");
+
+        }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+
         }
     }
 }
