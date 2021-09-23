d7bbd9b82ae30aca6c6c92290d694614df3ca4d8
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-09-23 13:47:54 -0700

    resetting for unit 7

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Collections.Generic;
 
-
 namespace files_module
 {
     class Program
@@ -12,31 +11,24 @@ namespace files_module
             var currentDirectory = Directory.GetCurrentDirectory();
             var storesDirectory = Path.Combine(currentDirectory, "stores");
             var salesFiles = FindFiles(storesDirectory);
-
             foreach (var file in salesFiles)
             {
                 Console.WriteLine(file);
             }
-
         }
 
         static IEnumerable<string> FindFiles(string folderName)
         {
             List<string> salesFiles = new List<string>();
-
-            var foundFiles = Directory.EnumerateFiles(folderName, "*.json", SearchOption.AllDirectories);
-
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
             foreach (var file in foundFiles)
             {
-                // var extenstion = Path.GetExtension(file);
-                // if (extenstion == ".json")
-                // {
+                var extension = Path.GetExtension(file);
+                if (extension == ".json")
                     salesFiles.Add(file);
-                // }
             }
 
             return salesFiles;
-
         }
     }
 }
